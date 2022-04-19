4d44831b4b14a60ee2d5f09519fe1dba0fe5a874
unknown <kosinskiy.andrey@ukr.net>
2022-04-19 17:36:55 +0300

    t1

--- internal/app/config.go
+++ internal/app/config.go
@@ -20,12 +20,12 @@ func NewConfig() *Config {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
-	port := ":" + os.Getenv("PORT")
-	debugLevel, err := strconv.Atoi(os.Getenv("DEBUG_LEVEL"))
+	port := ":" + os.Getenv("APP_PORT")
+	debugLevel, err := strconv.Atoi(os.Getenv("APP_DEBUG_LEVEL"))
 	if err != nil {
 		log.Fatalf("Can`t parse config field debugLevel: %s", err)
 	}
-	dbURL := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PASSWORD"), os.Getenv("DATABASE_HOST"), os.Getenv("DATABASE_NAME"), os.Getenv("DATABASE_SSL_MODE"))
+	dbURL := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", os.Getenv("APP_DATABASE_USER"), os.Getenv("APP_DATABASE_PASSWORD"), os.Getenv("APP_DATABASE_HOST"), os.Getenv("APP_DATABASE_NAME"), os.Getenv("APP_DATABASE_SSL_MODE"))
 	fmt.Println("Database URL: ", dbURL)
 	return &Config{port, debugLevel, dbURL}
 }
