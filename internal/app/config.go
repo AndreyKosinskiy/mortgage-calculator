22cee5038d5215cbeac86dfb59df1bf3279eeda0
unknown <kosinskiy.andrey@ukr.net>
2022-04-19 18:04:26 +0300

    add addon in heroku

--- internal/app/config.go
+++ internal/app/config.go
@@ -25,7 +25,7 @@ func NewConfig() *Config {
 	if err != nil {
 		log.Fatalf("Can`t parse config field debugLevel: %s", err)
 	}
-	dbURL := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", os.Getenv("APP_DATABASE_USER"), os.Getenv("APP_DATABASE_PASSWORD"), os.Getenv("APP_DATABASE_HOST"), os.Getenv("APP_DATABASE_NAME"), os.Getenv("APP_DATABASE_SSL_MODE"))
+	dbURL := os.Getenv("HEROKU_POSTGRESQL_CYAN_URL") //fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", os.Getenv("APP_DATABASE_USER"), os.Getenv("APP_DATABASE_PASSWORD"), os.Getenv("APP_DATABASE_HOST"), os.Getenv("APP_DATABASE_NAME"), os.Getenv("APP_DATABASE_SSL_MODE"))
 	fmt.Println("Database URL: ", dbURL)
 	return &Config{port, debugLevel, dbURL}
 }
